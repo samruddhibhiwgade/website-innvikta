11f9d7252f0fcd12496f4811e21e292bf7d5cd08
samruddhibhiwgade <samruddhi96583@gmail.com>
2026-07-25 15:35:07 +0530

    fix: suppress inline PHP warnings in forms_api.php to prevent invalid JSON response parse errors

--- Cyberhelp_Innvikta/server/forms_api.php
+++ Cyberhelp_Innvikta/server/forms_api.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 0);
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
 require __DIR__ . '/config.php';
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
